5bf339aa89df25c83a540bc2f435789e03aca3be
Felipe Rodrigues <felipe.fcr9@gmail.com>
2021-06-13 16:39:26 -0300

    Conteudo de Avaliacao de ARTIN

---
+++ routes/pedidos.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+
+
+// RETORNA TODOS OS PEDIDOS
+router.get('/', (req, res, next) => {
+    res.status(200).send({
+        mensagem: 'Retorna os pedidos'
+    });
+})
+
+
+// INSERE UM PEDIDO
+router.post('/', (req, res, next) => {
+    const pedido = {
+        id_produto: req.body.id_produto,
+        nome: req.body.nome,
+        quantidade: req.body.quantidade,
+        preco: req.body.preco,
+        descricao: req.body.descricao,
+        imagem: req.body.imagem
+    }
+    res.status(201).send({
+        mensagem:'O pedido foi criado',
+        pedidoCriado: pedido
+    });
+});
+
+
+// RETORNA OS DADOS DE UM PEDIDO
+router.get('/:id_pedido', (req, res, next) => {
+    const id = req.params.id_pedido
+        res.status(200).send({
+            mensagem: 'Detalhes do pedido',
+            id_pedido: id
+    });   
+})
+        
+
+//EXCLUI UM PRODUTO
+router.delete('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem:'Pedido excluido'
+    });
+});
+
+module.exports = router;
